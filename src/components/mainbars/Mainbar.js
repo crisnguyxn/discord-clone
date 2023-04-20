f40e7f415ebdfeec0380add93740247ea9c172b3
crisnguyxn <hyundev1406@gmail.com>
2023-04-20 19:52:56 +0700

    change sth

--- src/components/mainbars/Mainbar.js
+++ src/components/mainbars/Mainbar.js
@@ -16,6 +16,7 @@ import soundWave from "../../image/sound-waves.png";
 import startup from "../../image/startup.png";
 import music from "../../image/music.png";
 import call from "../../image/call.png";
+import { socket } from "../../socket/socket";
 
 function Mainbar(props) {
   const [roomId] = useContext(ServerContext);
@@ -23,6 +24,42 @@ function Mainbar(props) {
   const [peers, setPeers] = useState([]);
   const tokenVal = document.cookie.split(";");
   const token = tokenVal[0].split("=")[1];
+  const [active, setActive] = useState(false);
+  const userStatus = {
+    username: localStorage.getItem("username"),
+    mic: true,
+    headphone: true,
+    online: true,
+    userId: localStorage.getItem("userId"),
+  };
+
+  const toggleMic = () => {
+    userStatus.mic = !userStatus.mic;
+    socket.emit("userInformation", userStatus);
+    const micTag = document.getElementById("contextMic");
+    if (userStatus.mic) {
+      return (micTag.className = "context mic");
+    } else {
+      micTag.classList.remove("context");
+      micTag.classList.remove("mic");
+    }
+  };
+
+  const toggleMute = () => {
+    userStatus.headphone = !userStatus.headphone;
+    socket.emit("userInformation", userStatus);
+    const muteTag = document.getElementById("contextMute");
+    if (userStatus.headphone) {
+      return (muteTag.className = "context headphone");
+    } else {
+      muteTag.classList.remove("context");
+      muteTag.classList.remove("headphone");
+    }
+  };
+
+  const toggleOnline = () => {
+    userStatus.online = !userStatus.online;
+  };
   const getRoom = async () => {
     if (roomId !== undefined) {
       const data = await axios.get(`${keys.BASE_URL}/rooms/room/${roomId}`, {
@@ -34,8 +71,51 @@ function Mainbar(props) {
     }
   };
   const joinRoom = (id) => {
-    props.getChannelId(id)
-  }
+    userStatus.online = !userStatus.online;
+    socket.emit("userInformation", userStatus);
+    mainFunction(1000);
+    props.getChannelId(id);
+  };
+  const mainFunction = (time) => {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      let mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      setActive(mediaRecorder.stream.active);
+      console.log(mediaRecorder);
+      var audioChunk = [];
+      mediaRecorder.addEventListener("dataavailable", function (event) {
+        audioChunk.push(event.data);
+      });
+      mediaRecorder.addEventListener("stop", () => {
+        let audioBlob = new Blob(audioChunk);
+        audioChunk = [];
+        let fileReader = new FileReader();
+        fileReader.readAsDataURL(audioBlob);
+        fileReader.onloadend = () => {
+          let base64String = fileReader.result;
+          socket.emit("voice", base64String);
+        };
+
+        mediaRecorder.start();
+
+        setTimeout(() => {
+          mediaRecorder.stop();
+        }, time);
+      });
+      setTimeout(() => {
+        mediaRecorder.stop();
+      }, time);
+    });
+    socket.on("send", (data) => {
+      let audio = new Audio(data);
+      audio.play();
+    });
+
+    socket.on("userUpdated", (data) => {
+      console.log(data);
+    });
+  };
+
   const getVoiceRoom = async () => {
     const data = await axios.get(`${keys.BASE_URL}/discord-rooms/${roomId}`);
     setPeers(data.data);
@@ -73,7 +153,7 @@ function Mainbar(props) {
                       className="voice-room"
                       onClick={() => joinRoom(peer._id)}
                     >
-                      <div  className="room-name">
+                      <div className="room-name">
                         <img src={dis} alt="" />
                         <p>{peer.name}</p>
                       </div>
@@ -98,12 +178,16 @@ function Mainbar(props) {
           </div>
           <div className="user-infos">
             <div className="actions">
-              <div className="connected-line">
-                <div className="noti">
-                  <img src={soundWave} alt="" /> Connected
+              {active ? (
+                <div className="connected-line">
+                  <div className="noti">
+                    <img src={soundWave} alt="" /> Connected
+                  </div>
+                  <img src={call} onClick={() => toggleOnline()} alt="" />
                 </div>
-                <img src={call} alt="" />
-              </div>
+              ) : (
+                " "
+              )}
               <div className="action">
                 <img src={videoCamera} alt="" />
                 <img src={share} alt="" />
@@ -117,8 +201,10 @@ function Mainbar(props) {
                 <p>{localStorage.getItem("username")}</p>
               </div>
               <div className="info-right">
-                <img src={mic} alt="" />
-                <img src={headphone} alt="" />
+                <img src={mic} onClick={() => toggleMic()} alt="" />
+                <span id="contextMic"></span>
+                <img onClick={() => toggleMute()} src={headphone} alt="" />
+                <span id="contextMute"></span>
                 <img src={settings} alt="" />
               </div>
             </div>
